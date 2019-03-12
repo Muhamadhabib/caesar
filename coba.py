a9373bc42e15f46ca81b226fc973e10f6e2b17ac
Muhamadhabib <muhamad.habib08@gmail.com>
2019-03-12 19:24:31 +0700

    tambah

--- coba.py
+++ coba.py
@@ -1,12 +1,12 @@
 list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-p = "habib"
+p = "habibz"
 c = ""
 for i in range(len(p)):
     a = (list.index(p[i])+3)%26
     c = c + list[a]
 print(c)
 
-filename = "D:\\CODE\\python\\text.txt"
+filename = "D:\CODE\python\\text.txt"
 file = open(filename,"r")
 for line in file:
     print(line)
