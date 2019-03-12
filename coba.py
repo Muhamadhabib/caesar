6e40a0d0152c9220b096d77273faf28af989d09e
Muhamadhabib <muhamad.habib08@gmail.com>
2019-03-12 20:19:42 +0700

    ketiga

--- coba.py
+++ coba.py
@@ -1,12 +1,17 @@
-list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-p = "habibz"
+list = ["a","b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+p = "udinus"
 c = ""
 for i in range(len(p)):
-    a = (list.index(p[i])+3)%26
+    a = (list.index(p[i])+13)%26
     c = c + list[a]
 print(c)
+nm = ""
+for line in range(25):
+    for l in range(len(c)):
+        de = (list.index(c[l])-line)%26
+        nm = nm + list[de]
+        print(nm)
+print(nm)
 
-filename = "D:\CODE\python\\text.txt"
-file = open(filename,"r")
-for line in file:
-    print(line)
+
+
