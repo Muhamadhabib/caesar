d102194426290585ab7e2d9860e10985de42a06b
alfanaisy <alfanaisy7@gmail.com>
2019-03-20 15:03:15 +0700

    commit 6 (alfan)

---
+++ coba.py
@@ -0,0 +1,26 @@
+list = ["a","b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+p = "habib"
+c = ""
+d = ""
+print("plantext : "+p)
+for i in range(len(p)):
+    a = (list.index(p[i])+3)%26
+    c = c + list[a]
+print("enkripsi : "+c)
+for i in range(len(c)):
+    a = (list.index(c[i])-3)%26
+    d = d + list[a]
+print("dekripsi : "+d)
+nm = ""
+no = 0
+for line in range(25):
+    for l in range(len(c)):
+        de = (list.index(c[l])-line)%26
+        no = no + 1
+        if no % len(c) == 0:
+                nm = nm + list[de] + "\n"
+        else: 
+                nm = nm + list[de]
+print(nm)
+
+
